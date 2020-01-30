84c07ea5f431dc6db5eea6b5057e1a05d0cca250
Tim Hobson <timhobson85@gmail.com>
2020-01-30 12:39:12 +1100

    finished the search forms

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -34,12 +34,66 @@ class SearchResults extends React.Component {
     .catch( err => console.warn('it broke', err) );
   }
 
+  componentDidUpdate(prevProps){
+    console.log(`update!`);
+    const oldQuery = prevProps.match.params
+    console.log('oldQuery:', oldQuery);
+    const newQuery = this.props.match.params
+    console.log('newQuery:', newQuery);
+    if (newQuery === oldQuery) {
+      return
+    }
+    let flightFrom = this.props.match.params.from
+    let flightTo = this.props.match.params.to
+
+    axios.get( URL )
+    .then( res => {
+      // console.log(res.filter(flight => flight.destination.includes( this.state.flightTo )))
+      let results = res.data
+      let filtered = results.filter(flight => flight.destination.includes( flightTo ) && flight.origin.includes( flightFrom ))
+      this.setState({flights: filtered})
+      console.log('state.flights:', this.state.flights);
+    } )
+    .catch( err => console.warn('it broke', err) );
+  }
+
+  // componentDidUpdate(prevProps, prevState, snapshot) {
+// const {match: {params: {query: searchTermOld}}} = prevProps;
+// const {match: {params: {query: searchTermNew}}} = this.props;
+// if (searchTermOld.toLowerCase() === searchTermNew.toLowerCase()) {
+//   return;
+// }
+// this.performSearch(searchTermNew.toLowerCase());
+// }
+
   render(){
-    return this.state.flights.map( flights =>
-      <div className="searchResults" key={flights.id}>
-        <p>{flights.date} - {flights.origin} - {flights.destination} - {flights.flightnum} - {flights.airplane.name}</p><button onClick={() => this.handleClick(flights.flightnum)}>book flight</button>
+    return(
+      <div>
+        <h2>search results</h2>
+
+        {
+
+          this.state.flights.length === 0
+          ?
+          <h3>no results</h3>
+          :
+          this.state.flights.map( flights =>
+            <div className="searchResults" key={flights.id}>
+              <p>{flights.date} - {flights.origin} - {flights.destination} - {flights.flightnum} - {flights.airplane.name}</p><button onClick={() => this.handleClick(flights.flightnum)}>book flight</button>
+            </div>)
+
+        }
+
       </div>
-    ) // return
+    )
+
+
+
+    // return this.state.flights.map( flights =>
+    //   <div className="searchResults" key={flights.id}>
+    //     <p>{flights.date} - {flights.origin} - {flights.destination} - {flights.flightnum} - {flights.airplane.name}</p><button onClick={() => this.handleClick(flights.flightnum)}>book flight</button>
+    //   </div>
+    // ) // return
   } // render
 
 } // Class
